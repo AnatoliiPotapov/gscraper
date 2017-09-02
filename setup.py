70bb6c7ee4763e96132bf56ba4817641897ed6e9
anatoly <anatolii.s.potapov@gmail.com>
2017-09-02 12:53:53 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gscrapper',
+      version='0.1',
+      description='Let you scrape images from google photo',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
